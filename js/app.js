7af3ecac4b962c9f8bd011cbb11d0046d87fe5b1
Hirenkunar <hireneelpro@yahoo.com>
2023-12-17 17:55:45 -0500

    File save changes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-const navbar = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("#close-btn");
-const sidebar = document.querySelector("#sidebar");
+const navbar = document.querySelector(".navbar");
+const menuIcon = document.querySelector(".menu-icon");
+const sidebarClose = document.querySelector(".sidebar-close");
+const sidebar = document.querySelector(".sidebar");
 const date = document.querySelector("#date");
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
@@ -12,11 +12,11 @@ window.addEventListener("scroll", function () {
   }
 });
 // show sidebar
-navBtn.addEventListener("click", function () {
+menuIcon.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
 });
-closeBtn.addEventListener("click", function () {
+sidebarClose.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
 });
 // set year
-date.innerHTML = new Date().getFullYear();
+// date.innerHTML = new Date().getFullYear();
